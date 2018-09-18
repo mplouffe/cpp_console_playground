121d6aabe1d46d10ebe1ed5fc8e703dd4d4590d9
mplouffe <matheuplouffe@gmail.com>
2018-09-17 22:19:33 -0500

    Added function to display data sizes for curiosity sake.

--- hello.cpp
+++ hello.cpp
@@ -2,17 +2,33 @@
 #include <windows.h>
 
 void ClearScreen();
-
+void DisplayDataSizes();
 
 int main()
 {
-    std::cout << "Hello World!\n";
     char response;
-    std::cin >> response;
-    ClearScreen();
-    std::cout << "Hello World Again!\n";
+    DisplayCard();
     std::cin >> response;
     return 0;
+}
+
+// Just curious about the datasizes on this system
+void DisplayDataSizes()
+{
+    std::cout << "The size of an int is:\t\t"
+        << sizeof(int) << " bytes." << std::endl;
+    std::cout << "The size of a short int is:\t"
+        << sizeof(short) << " bytes." << std::endl;
+    std::cout << "The size of a long int is:\t"
+        << sizeof(long) << " bytes." << std::endl;
+    std::cout << "The size of a char is:\t\t"
+        << sizeof(char) << " bytes." << std::endl;
+    std::cout << "The size of a float is:\t\t"
+        << sizeof(float) << " bytes." << std::endl;
+    std::cout << "The size of a double is:\t"
+        << sizeof(double) << " bytes." << std::endl;
+    std::cout << "The size of a bool is:\t\t"
+        << sizeof(bool) << " bytes." << std::endl;
 }
 
 void ClearScreen()
@@ -50,4 +66,32 @@ void ClearScreen()
 
     /* Move the cursor home */
     SetConsoleCursorPosition( hStdOut, homeCoords );
+}
+
+void DisplayBoard()
+{
+    HANDLE              hStdOut;
+    COORD               currentCoOrds = {0, 0};
+
+    hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    if (hStdOut == INVALID_HANDLE_VALUE) return;
+
+
+}
+
+void DisplayCard(HANDLE hStdOut, COORD currentCoOrds)
+{
+    int currentColumn = currentCoOrds.Y + 2;
+    int currentRow = currentCoOrds.X + 2;
+
+    SetConsoleCursorPosition(hStdOut, currentCoOrds);
+    for(int i = 0; i < 7; i += 1) {
+        std::cout << "*";
+    }
+
+    currentCoOrds = {currentColumn, currentRow};
+    SetConsoleCursorPosition(hStdOut, currentCoOrds);
+    for(int i = 0; i < 7; i += 1) {
+        std::cout << "*";
+    }
 }
